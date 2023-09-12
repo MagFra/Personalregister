092c67bae1f2c117b7bfcc4abef4e42d246368ec
MagFra <47413425+magfra@users.noreply.github.com>
2023-09-12 17:06:54 +0200

    Nya kommentarer.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Personalregister
+﻿using System.Runtime.CompilerServices;
+
+namespace Personalregister
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            private Personer personer = new Personer;
-    }
+
+            Console.WriteLine("");
+
+            // Jag begriper absolut inte varför jag inte får instansiera ett nytt objekt av typen "Personer" här.
+            // får bara helt obegripliga felmeddelanden hela riden!
+
+            /*
+            private Personer personer = new Personer();
+            // fösök att skapa en ny person.
+            personer.skapaNyPerson("Magnus","Fransson");
+            */
+        }
     }
 }
